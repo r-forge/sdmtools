52ed2d9d447b88780707e8612e6ef205e7587d8b
jjvanderwal (Dr. ) <jjvanderwal@gmail.com>
2010-04-15 06:30:21 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sdmtools@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ working.dir/optimize/sann.c
@@ -0,0 +1,38 @@
+/* File: sann.c */ 
+/*
+this is is modifying the sann optimization code from the optim package to 
+permit selection of new points with an n that differs from the starting set.
+
+*/
+
+#include <R.h> 
+#include <Rinternals.h>
+#include <R_ext/Applic.h>
+
+SEXP trial(SEXP p, SEXP fn, SEXP rho) {
+	//define the function and output
+	SEXP R_fcall;
+    int i;
+	int n = LENGTH(p); //get the length of p
+	SEXP s, x;
+	PROTECT_INDEX ipx; //not sure what this does
+	PROTECT(p = coerceVector(p,INTSXP));
+	int *tp = INTEGER(p);
+    PROTECT(x = allocVector(REALSXP, n)); //create a vector the same length as p
+	PROTECT(R_fcall = lang2(fn, R_NilValue));
+	
+	double *tx;
+	tx = REAL(x);
+	
+    for (i = 0; i < n; i++) { //copy data ensuring it is finite
+		tx[i] = tp[i];
+    }
+
+    SETCADR(R_fcall, x);
+    PROTECT_WITH_INDEX(s = eval(R_fcall, rho), &ipx);
+    REPROTECT(s = coerceVector(s, REALSXP), ipx);
+    UNPROTECT(2);
+    return s;
+
+}
+
