adc34350fea1a37c05f755f69ada6d86b021fb3d
jjvanderwal (Dr. ) <jjvanderwal@gmail.com>
2011-03-21 05:35:48 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sdmtools@92 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- Rdev/slope_and_aspect/slope.aspect.c
+++ Rdev/slope_and_aspect/slope.aspect.c
@@ -1,10 +1,81 @@
-/* File: Functions.c */ 
+/* File: slope.aspect.c */ 
 /*
-####????
-
+this calculates slope and aspect of a raster based on ArcGIS 9.3 documentation
+http://webhelp.esri.com/arcgiSDEsktop/9.3/index.cfm?TopicName=How%20Slope%20works
+http://webhelp.esri.com/arcgisdesktop/9.3/index.cfm?TopicName=How%20Aspect%20(3D%20Analyst)%20works
 */
 
 #include <R.h>
 #include <Rinternals.h>
 #include <Rmath.h>
 #include <Rdefines.h>
+
+/*
+The Slope algorithm
+The rate of change (delta) of the surface in the horizontal (dz/dx) and vertical (dz/dy) directions from the center cell determines the slope. The basic algorithm used to calculate the slope is:
+slope_radians = ATAN ( sqrt ( [dz/dx]2 + [dz/dy]2 ) )
+
+Slope is commonly measured in degrees, which uses the algorithm:
+slope_degrees = ATAN ( sqrt ( [dz/dx]2 + [dz/dy]2 ) ) * 57.29578
+
+The slope algorithm can also be interpreted as:
+slope_degrees = ATAN (rise_run) * 57.29578 
+where: 
+rise_run = sqrt ( [dz/dx]2 + [dz/dy]2 ] )
+
+The values of the center cell and its eight neighbors determine the horizontal and vertical deltas. The neighbors are identified as letters from 'a' to 'i', with 'e' representing the cell for which the aspect is being calculated.
+a b c
+d e f
+g h i
+
+The rate of change in the x direction for cell 'e' is calculated with the algorithm:
+[dz/dx] = ((c + 2f + i) - (a + 2d + g) / (8 * x_cell_size)
+
+The rate of change in the y direction for cell 'e' is calculated with the following algorithm:
+[dz/dy] = ((g + 2h + i) - (a + 2b + c)) / (8 * y_cell_size)
+
+Burrough, P. A. and McDonell, R.A., 1998. Principles of Geographical Information Systems (Oxford University Press, New York), p. 190.
+*/
+
+
+SEXP slope (SEXP x, SEXP widths, SEXP heights) {
+    //define the pointers for the data
+	PROTECT(x = coerceVector(x, REALSXP)); //data values in the matrix
+	PROTECT(widths = coerceVector(widths, REALSXP)); //data values in the matrix
+	PROTECT(heights = coerceVector(heights, REALSXP)); //data values in the matrix
+	double *data = REAL(x), *width = REAL(widths), *height = REAL(heights); //create pointers to the data
+	int *dims = INTEGER(coerceVector(getAttrib(x, R_DimSymbol), INTSXP)); //get the dimension of the input matrix
+    int nrows = dims[0]; int ncols = dims[1]; //assign the number of rows and columns in the matrix
+
+	SEXP ans; PROTECT(ans = allocMatrix(REALSXP, nrows, ncols)); ;//setup the output
+	double *out = REAL(ans); //pointer to output dataset
+	
+	double a,b,c,d,f,e,g,h,i; //neighboring cell values
+	
+	//cycle through the data of input matrix & calculate the slope
+	for (int row=0; row<nrows; row++){
+        for (int col=0; col<ncols; col++){
+			if (ISNA(data[row+nrows*col])) { out[row+nrows*col] = NA_REAL; //if input is NA, set output to NA
+			} else { //calculate the slope
+				e = data[row+nrows*col]; // define the value of the current cell
+				//define the values from the neighboring cells
+				a = (row==0 || col==0 || ISNA(data[(row-1)+nrows*(col-1)])) ? e : data[(row-1)+nrows*(col-1)]; 
+				b = (row==0 || ISNA(data[(row-1)+nrows*col])) ? e : data[(row-1)+nrows*col]; 
+				c = (row==0 || col==ncols-1 || ISNA(data[(row-1)+nrows*(col+1)])) ? e : data[(row-1)+nrows*(col+1)];
+				d = (col==0 || ISNA(data[row+nrows*(col-1)])) ? e : data[row+nrows*(col-1)];
+				f = (col==ncols-1 || ISNA(data[row+nrows*(col+1)])) ? e : data[row+nrows*(col+1)];
+				g = (row==nrows-1 || col==0 || ISNA(data[(row+1)+nrows*(col-1)])) ? e : data[(row+1)+nrows*(col-1)]; 
+				h = (row==nrows-1 || ISNA(data[(row+1)+nrows*col])) ? e : data[(row+1)+nrows*col]; 
+				i = (row==nrows-1 || col==ncols-1 || ISNA(data[(row+1)+nrows*(col+1)])) ? e : data[(row+1)+nrows*(col+1)];
+				//get the rise_run
+				double dz_dx = ((c+2*f+i) - (a+2*d+g)) / (8*width[row]);
+				double dz_dy = ((g+2*h+i) - (a+2*b+c)) / (8*height[row]);
+				out[row+nrows*col] = sqrt( dz_dx * dz_dx + dz_dy * dz_dy ); //store the rise/run info
+			}
+        }
+    } 
+	//return the output data
+	UNPROTECT(4);
+    return(ans); 
+}
+
--- Rdev/slope_and_aspect/trial.R
+++ Rdev/slope_and_aspect/trial.R
@@ -4,13 +4,41 @@
 ################################################################################
 ####################################################################################
 #required to build code OUTSIDE R
-cd /home1/31/jc165798/SCRIPTS/sdmcode/misc/ben.phillips/individual.genetic.models/
+cd /home1/31/jc165798/SCRIPTS/sdmtools/Rdev/slope_and_aspect/
 
-R CMD SHLIB Functions.c
+R CMD SHLIB slope.aspect.c
 
 
 ####################################################################################
 #START R
 #the function
-dyn.load("/home1/31/jc165798/SCRIPTS/sdmcode/misc/ben.phillips/individual.genetic.models/Functions.so")
-system.time( .Call('mother',R_n=10000,R_spX=10,R_spY=10,R_lambda=5.0,R_K=1000,R_disp_cost=0,R_reports_rate=10,R_num_gens=100,rho=environment()) )
+dyn.load("/home1/31/jc165798/SCRIPTS/sdmtools/Rdev/slope_and_aspect/slope.aspect.so")
+library(SDMTools)
+tasc = as.asc(matrix(1:100,nr=10,by.row=TRUE),yll=75); print(tasc)
+slope = function(mat,latlon=FALSE) {
+	#check input for class for returning info
+	if (any(class(mat) == 'asc')) { attrib = attributes(mat)
+	} else if (any(class(mat) %in% 'RasterLayer')) { attrib = mat; mat = asc.from.raster(mat)
+	} else if (any(class(mat) == 'SpatialGridDataFrame')) { attrib = mat; mat = asc.from.sp(mat)
+	} else { attrib = attributes(mat) }
+	if (!any(class(mat) == 'asc')) { stop('objects must be of class "asc"') } #check to ensure asc 
+	
+	# get the cell size information
+	if (latlon) {
+		tt = grid.info(getXYcoords(mat)$y,attr(mat,'cellsize')) #if latlon = true get the length & width of cells
+		width = rowMeans(cbind(tt$top,tt$bottom))/1000; height = tt$side/1000 #get the width and height of the cells in km (NOT m)
+	} else { width = height = rep(attr(mat,'cellsize'),length(getXYcoords(mat)$y)) } #get the cell width & height 
+	
+	slope = .Call('slope',mat,width,height) #get the slope information
+	mat[,] = slope[,] #move all slope info to mat
+	
+	#reset the attributes of the input
+	if (any(class(attrib) %in% 'RasterLayer')) { attrib = setValues(attrib, as.vector(t(t(unclass(mat))[dim(mat)[2]:1,]))); return(attrib)
+	} else if (any(class(attrib) == 'SpatialGridDataFrame')) { attrib@data[1] = as.vector(unclass(mat)[,dim(mat)[2]:1]); return(attrib)
+	} else { attributes(mat) = attrib; return(mat) }
+}
+tasc[,]
+system.time({ tt = slope(tasc,latlon=TRUE) })
+tt[,]
+
+system.time( .Call('aspect',tasc) )
